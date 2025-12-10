4370c8c5696902caf052298c8081880d60a504a4
Wayne Lee <waynel1996weiyang@gmail.com>
2025-12-09 21:31:54 -0800

    Fix issue with gym environment implementation.

--- gymnasium_cus_env/gymnasium_env/envs/marble_game.py
+++ gymnasium_cus_env/gymnasium_env/envs/marble_game.py
@@ -11,22 +11,25 @@ PLAYER_SPEED = 0.5
 PLAYER_ROT_SPEED = 0.1
 PLAYER_SIZE = (16, 16)
 PLAYER_HEAD_SIZE = (4, 4)
+PLAYER_SHOOTING_COOLDOWN = 0.5
 
 ENEMY_SPEED = 0.2
 ENEMY_ROT_SPEED = 0.1
 ENEMY_SIZE = (8, 8)
 ENEMY_HEAD_SIZE = (2, 2)
 
-BULLET_SPEED = 2
+BULLET_SPEED = 2.0
 BULLET_SIZE = (4, 4)
+BULLET_LIFESPAN = 10.0
 
 RAY_CAST_COUNT = 32
+RAY_CAST_SECTION = 5
 
 SCREEN_SIZE = (512, 512)
-STARTING_ANGLE = math.pi / 2.0
-BASE_SPAWN_RATE = 5
+STARTING_ANGLE = math.pi
+BASE_SPAWN_RATE = 20.0
 BASE_SPAWN_AMOUNT = 1
-MIN_SPAWN_RATE = 0.1
+MIN_SPAWN_RATE = 0.5
 
 class BaseEntity:
   def __init__(self, size, pos, angle, speed, rot_speed):
@@ -39,21 +42,21 @@ class BaseEntity:
 class PlayerEntity(BaseEntity):
   def __init__(self, size, pos, angle, speed, rot_speed):
     BaseEntity.__init__(self, size, pos, angle, speed, rot_speed)
-    self.cooldown_start = 0
-    self.cooldown = 0.1
+    self.cooldown_start = 0.0
+    self.cooldown = PLAYER_SHOOTING_COOLDOWN
 
 class EnemyEntity(BaseEntity):
   def __init__(self, size, pos, angle, speed, rot_speed):
     BaseEntity.__init__(self, size, pos, angle, speed, rot_speed)
 
 class BulletEntity(BaseEntity):
-  def __init__(self, size, pos, angle, speed, lifespan = 1):
-    BaseEntity.__init__(self, size, pos, angle, speed, 0)
+  def __init__(self, size, pos, angle, speed, lifespan = BULLET_LIFESPAN):
+    BaseEntity.__init__(self, size, pos, angle, speed, 0.0)
     self.lifespan = lifespan
     self.start = time.time()
 
 class MarbleGameEnv(gym.Env):
-  metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+  metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 64}
   
   def __init__(self, render_mode=None, size=5):
     self.size = size  # The size of the square grid
@@ -62,8 +65,8 @@ class MarbleGameEnv(gym.Env):
     # Observation space 
     self.observation_space = spaces.Dict(
       {
-        "position": spaces.Box(0, self.window_size - 1, shape = (2, ), dtype = np.int32),
-        "angle": spaces.Box(-1, 1, shape = (2, ), dtype = np.float32),
+        "position": spaces.Box(0, self.window_size - 1, shape = (2, ), dtype = np.float64),
+        "angle": spaces.Box(-1, 1, shape = (2, ), dtype = np.float64),
         "ray_cast": spaces.Box(0, 1, shape = (RAY_CAST_COUNT, 5), dtype = np.bool)
       }
     )
@@ -78,10 +81,10 @@ class MarbleGameEnv(gym.Env):
     self.clock = None
 
   def _get_obs(self):
-    return {"position": self._player.pos, "angle": np.array([math.cos(self._player.angle), math.sin(self._player.angle)], dtype = np.float32), "ray_cast": self._player_ray_cast}
+    return {"position": self._player.pos, "angle": np.array([math.sin(self._player.angle), math.cos(self._player.angle)], dtype = np.float32), "ray_cast": self._player_ray_cast}
 
   def _get_info(self):
-    return {"player":self._player, "start_time": self._start_time, "enemy_list": self._enemy_list, "bullet_list": self._bullet_list, "enemy_last_spawn": self._enemy_last_spawn}
+    return {"player":self._player, "start_time": self._start_time, "enemy_list": self._enemy_list, "bullet_list": self._bullet_list, "enemy_last_spawn": self._enemy_last_spawn, "ray_cast_points": self._ray_cast_points}
 
   def reset(self, seed = None, options = None):
     super().reset(seed = seed)
@@ -95,6 +98,7 @@ class MarbleGameEnv(gym.Env):
     self._enemy_list = set([])
     self._bullet_list = set([])
     self._enemy_last_spawn = None
+    self._ray_cast_points = np.asarray([[(0,0)] * 6] * 32)
 
     observation = self._get_obs()
     info = self._get_info()
@@ -104,17 +108,17 @@ class MarbleGameEnv(gym.Env):
 
     return observation, info
   
-  def _random_edge_pos():
+  def _random_edge_pos(self):
     random_edge = random.randrange(4)
     random_pos = random.randrange(SCREEN_SIZE[random_edge % 2])
     random_coord = np.array([random_pos if random_edge % 2 == i else SCREEN_SIZE[i] * (random_edge // 2) for i in range(2)])
 
     return random_coord
   
-  def _get_entity_collision(entity : BaseEntity):
+  def _get_entity_collision(self, entity : BaseEntity):
     return (entity.pos[0] - (entity.size[0] / 2), entity.pos[0] + (entity.size[0] / 2), entity.pos[1] - (entity.size[1] / 2), entity.pos[1] + (entity.size[1] / 2))
 
-  def _check_collision(colli_1, colli_2):
+  def _check_collision(self, colli_1, colli_2):
     if colli_1[0] > colli_2[1] or colli_1[1] < colli_2[0]:
       return False
     
@@ -124,7 +128,7 @@ class MarbleGameEnv(gym.Env):
     return True
   
   # Liang_barsky algorithm, but just return boolean of whether it intersect or not
-  def _check_line_collision(colli, line):
+  def _check_line_collision(self, colli, line):
     x_min, x_max, y_min, y_max = colli
     x1, y1, x2, y2 = line
 
@@ -136,9 +140,9 @@ class MarbleGameEnv(gym.Env):
     t_exit = 1.0
 
     for i in range(4):
-      if p[i] == 0:  # Check if line is parallel to the clipping boundary -1/-2 3/2 2/1 1/-1
+      if p[i] == 0:  # Check if line is parallel to the clipping boundary
         if q[i] < 0:
-          return False  # Line is outside and parallel, so completely discarded 0.5 1.0 0.0 1.0 
+          return False  # Line is outside and parallel, so completely discarded
       else:
         t = q[i] / p[i]
         if p[i] < 0:
@@ -177,9 +181,9 @@ class MarbleGameEnv(gym.Env):
     rotation = 0
   
     if action[4]:
-      rotation += PLAYER_ROT_SPEED
-    if action[5]:
       rotation -= PLAYER_ROT_SPEED
+    if action[5]:
+      rotation += PLAYER_ROT_SPEED
 
     self._player.angle = (self._player.angle + rotation) % (2 * math.pi)
 
@@ -187,8 +191,8 @@ class MarbleGameEnv(gym.Env):
     enemy_list_update = copy.deepcopy(self._enemy_list)
 
     for enemy in enemy_list_update:
-      enemy.angle = math.atan2(self._player.pos[1] - enemy.pos[1], self._player.pos[0] - enemy.pos[0])
-      enemy_move_direction = np.array([math.cos(enemy.angle), math.sin(enemy.angle)])
+      enemy.angle = math.atan2(self._player.pos[0] - enemy.pos[0], self._player.pos[1] - enemy.pos[1])
+      enemy_move_direction = np.array([math.sin(enemy.angle), math.cos(enemy.angle)])
 
       enemy.pos = np.add(enemy.pos, enemy_move_direction * enemy.speed)
 
@@ -196,17 +200,17 @@ class MarbleGameEnv(gym.Env):
     spawn_rate = max(MIN_SPAWN_RATE, BASE_SPAWN_RATE - (time_elapsed // 10) * 0.1)
     spawn_amount = BASE_SPAWN_AMOUNT + (time_elapsed // 60)
 
-    if not enemy_last_spawn or time.time() - enemy_last_spawn > spawn_rate:
+    if not self._enemy_last_spawn or time.time() - self._enemy_last_spawn > spawn_rate:
       for _ in range(int(spawn_amount)):
         random_coord = self._random_edge_pos()
         enemy_list_update.add(EnemyEntity(ENEMY_SIZE, random_coord, STARTING_ANGLE, ENEMY_SPEED, ENEMY_ROT_SPEED))
-      enemy_last_spawn = time.time()
+      self._enemy_last_spawn = time.time()
 
     # Bullet
     bullet_list_update = copy.deepcopy(self._bullet_list)
 
     for bullet in bullet_list_update:
-      bullet_move_direction = np.array([math.cos(bullet.angle), math.sin(bullet.angle)])
+      bullet_move_direction = np.array([math.sin(bullet.angle), math.cos(bullet.angle)])
 
       bullet.pos = np.add(bullet.pos, bullet_move_direction * bullet.speed)
 
@@ -226,24 +230,20 @@ class MarbleGameEnv(gym.Env):
     enemy_list_to_remove = set([])
     bullet_list_to_remove = set([])
 
-    ray_cast_line = []
+    ray_cast_points = []
     ray_cast_update = []
 
     for n in range(RAY_CAST_COUNT):
       angle = ((2 * math.pi) / RAY_CAST_COUNT) * n
 
-      ray_cast_line.append((self._player.pos[0], self._player.pos[1], 
-                          self._player.pos[0] + math.cos(angle) * 32, self._player.pos[1] + math.sin(angle) * 32)) # 0 - 32
-      ray_cast_line.append((self._player.pos[0] + math.cos(angle) * 32, self._player.pos[1] + math.sin(angle) * 32, 
-                          self._player.pos[0] + math.cos(angle) * 64, self._player.pos[1] + math.sin(angle) * 64)) # 32 - 64
-      ray_cast_line.append((self._player.pos[0] + math.cos(angle) * 64, self._player.pos[1] + math.sin(angle) * 64, 
-                          self._player.pos[0] + math.cos(angle) * 128, self._player.pos[1] + math.sin(angle) * 128)) # 64 - 128
-      ray_cast_line.append((self._player.pos[0] + math.cos(angle) * 128, self._player.pos[1] + math.sin(angle) * 128, 
-                          self._player.pos[0] + math.cos(angle) * 256, self._player.pos[1] + math.sin(angle) * 256)) # 128 - 256
-      ray_cast_line.append((self._player.pos[0] + math.cos(angle) * 256, self._player.pos[1] + math.sin(angle) * 256, 
-                          self._player.pos[0] + math.cos(angle) * 512, self._player.pos[1] + math.sin(angle) * 512)) # 256 - 512
+      ray_cast_points.append([(self._player.pos[0], self._player.pos[1]), 
+                              (self._player.pos[0] + math.sin(angle) * 32, self._player.pos[1] + math.cos(angle) * 32),
+                              (self._player.pos[0] + math.sin(angle) * 64, self._player.pos[1] + math.cos(angle) * 64),
+                              (self._player.pos[0] + math.sin(angle) * 128, self._player.pos[1] + math.cos(angle) * 128),
+                              (self._player.pos[0] + math.sin(angle) * 256, self._player.pos[1] + math.cos(angle) * 256),
+                              (self._player.pos[0] + math.sin(angle) * 512, self._player.pos[1] + math.cos(angle) * 512)])
       
-      ray_cast_update.extend([False, False, False, False, False])
+      ray_cast_update.append([False, False, False, False, False])
 
     for enemy in enemy_list_update:
       enemy_collision = self._get_entity_collision(enemy)
@@ -267,11 +267,15 @@ class MarbleGameEnv(gym.Env):
       if enemy_remove_flag:
         continue
 
-      for index, line in enumerate(ray_cast_line):
-        ray_cast_update[index] = ray_cast_update[index] or self._check_line_collision(enemy_collision, line)
+      for index, points in enumerate(ray_cast_points):
+        for section in range(RAY_CAST_SECTION):
+          ray_cast_update[index][section] = ray_cast_update[index][section] or self._check_line_collision(enemy_collision, points[section] + points[section + 1])
 
-    enemy_list_update = enemy_list_update - enemy_list_to_remove
-    bullet_list_update = bullet_list_update - bullet_list_to_remove
+    # Update saved variable
+    self._enemy_list = enemy_list_update - enemy_list_to_remove
+    self._bullet_list = bullet_list_update - bullet_list_to_remove
+    self._player_ray_cast = np.asarray(ray_cast_update)
+    self._ray_cast_points = ray_cast_points
 
     # Observation and Info
     observation = self._get_obs()
@@ -304,7 +308,7 @@ class MarbleGameEnv(gym.Env):
 
     player_rect = player_image.get_rect()
 
-    player_rotated = pygame.transform.rotate(player_image, math.degrees(self._player.angle))
+    player_rotated = pygame.transform.rotate(player_image, math.degrees(self._player.angle + math.pi))
     player_rect.center = self._player.pos
 
     canvas.blit(player_rotated, player_rect)
@@ -317,22 +321,30 @@ class MarbleGameEnv(gym.Env):
     for enemy in self._enemy_list:
       enemy_rect = enemy_image.get_rect()
 
-      enemy_rotated = pygame.transform.rotate(enemy_image, math.degrees(enemy.angle))
+      enemy_rotated = pygame.transform.rotate(enemy_image, math.degrees(enemy.angle + math.pi))
       enemy_rect.center = enemy.pos
 
       canvas.blit(enemy_rotated, enemy_rect)
 
     # Bullet image
-    bullet_image = pygame.Surface((4, 8), pygame.SRCALPHA)
+    bullet_image = pygame.Surface((4, 4), pygame.SRCALPHA)
     bullet_image.fill(pygame.Color(255, 255, 0))
 
     for bullet in self._bullet_list:
       bullet_rect = bullet_image.get_rect()
 
-      bullet_rotated = pygame.transform.rotate(bullet_image, math.degrees(bullet.angle))
+      bullet_rotated = pygame.transform.rotate(bullet_image, math.degrees(bullet.angle + math.pi))
       bullet_rect.center = bullet.pos
 
       canvas.blit(bullet_rotated, bullet_rect)
+
+    red_line_colour = (255, 0, 0)
+    green_line_colour = (0, 127, 0)
+      
+    for index, points in enumerate(self._ray_cast_points):
+      for section in range(RAY_CAST_SECTION):
+        line_colour = green_line_colour if self._player_ray_cast[index, section] else red_line_colour
+        pygame.draw.line(canvas, line_colour, points[section], points[section + 1])
 
     if self.render_mode == "human":
       # The following line copies our drawings from `canvas` to the visible window
--- test.py
+++ test.py
@@ -1,5 +1,49 @@
 import gymnasium
 import gymnasium_env
+import numpy as np
+import pygame
+import math
+
 env = gymnasium.make('gymnasium_env/MarbleGame-v0', render_mode="human")
 env.reset()
-env.render()
+
+done = False
+
+while not done:
+  action = np.random.randint(2, size = 7)
+  obs, reward, done, _, info = env.step(action)
+  # print(obs["ray_cast"])
+  # print(info)
+
+# pygame.init()
+# pygame.display.init()
+# window = pygame.display.set_mode((16, 16))
+
+# canvas = pygame.Surface((16, 16))
+# canvas.fill((255, 255, 255))
+
+# enemy_image = pygame.Surface((8, 8), pygame.SRCALPHA)
+# pygame.draw.circle(enemy_image, (127, 0, 0), (4, 4), 4)
+# pygame.draw.circle(enemy_image, (255, 255, 255), (4, 2), 2)
+
+# enemy_rect = enemy_image.get_rect()
+
+# enemy_angle = math.atan2(16 - 8, 0 - 8) # x, y
+
+# print(enemy_angle)
+# print(math.sin(enemy_angle), math.cos(enemy_angle))
+
+# test_angle = math.pi/2
+# print(math.sin(test_angle), math.cos(test_angle))
+
+# enemy_rotated = pygame.transform.rotate(enemy_image, math.degrees(enemy_angle + math.pi))
+# enemy_rect.center = (8, 8)
+
+# canvas.blit(enemy_rotated, enemy_rect)
+
+# window.blit(canvas, canvas.get_rect())
+# pygame.event.pump()
+# pygame.display.update()
+
+# while True:
+#   pass
